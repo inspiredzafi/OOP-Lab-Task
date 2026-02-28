3087b8da4f4a1d0279dfc3db48ee20be03f9ab15
Huzaifa <huzaifaofficial087@gmail.com>
2026-02-28 21:24:08 +0500

    Completed Task 4

--- Task4/task4.cpp
+++ Task4/task4.cpp
@@ -9,7 +9,7 @@
     1. Which function calls the copy constructor?
     2. Why does it happen?
     3. Trace the full execution order from start to finish.
-
+z
 */
 
 #include <iostream>
@@ -33,12 +33,15 @@ class Student
     }
 
     Student(Student& student)
+    : name("Default"), Department("Default")
     {
-        cout<< "The copy constructor was called " << endl ;
+        cout<< "The copy constructor was called and assigned the name: " << name << endl ;
+        
     }
 
     ~Student()
     {
+        cout<< "Destroyed the object with the name " << name << endl ;
     }
 };
 
@@ -61,9 +64,9 @@ int main()
 
     Student s1("Huzaifa", "Computer Science");
 
-    displayByRef(s1);
-    cout<< "Displaying by Reference" << endl ;
     displayByValue(s1);
+    cout<< "Displaying by Reference" << endl ;
+    displayByRef(s1);
 
         return 0;
 }
@@ -75,12 +78,17 @@ int main()
      Questions:
     1. Which function calls the copy constructor?
 
-        The copy constructor is called when the function "displayByRef" is called. 
+        The copy constructor is called when the function "displayByValue" is called. 
 
-    2. Why does it happen?
+    2. Why does it happen?  
+        When an object is passed to some function by value it creates a copy of this object for its internal use so that nothing is changed in the original variable.
 
     
     3. Trace the full execution order from start to finish.
-
-
+        
+        1. S1 is created with name <Huzaifa> and Department <Computer Science>
+        2. Function displayByValue is called, which internally calls the copy constructor for its internal use in the function.
+        3. The name and the department of the object is printed, and then destructor is called for the newly created object in the displayByValue function. (Named default intionally to differenciate between two obejects.)
+        4. Function displayByRef is called, the object is passed by reference and hence no copy constructor is called for this call.
+        5. The Student s1 with the name "Huzaifa" is destruced. Its destructor is called.
 */
